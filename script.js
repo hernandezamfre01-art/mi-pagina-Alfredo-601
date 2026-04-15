520eabdfb09960f9f35e36b49cf3dca8a6e646e2
hernandezamfre01-art <hernandezamfre01@gmail.com>
2026-04-15 13:40:06 -0600

    funcion

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function mostrarmensaje() {
+    let mensaje = document.getElementsByid("mensaje");
+
+    mensaje.innerHTML = " cuida y protege a los animales";
+    mensaje.style.display = "block";
+
+    setTimeout
+
+}
